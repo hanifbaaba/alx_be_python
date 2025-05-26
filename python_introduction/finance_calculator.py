d448b59492b8f5b2fcfb39e60f3e2a6a4018f0cf
hanifubaaba <baabahanif88@gmail.com>
2025-05-26 05:07:37 +0100

    Pushed finance calculator component

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
-income = float(input("Enter your monthly income: "))
-expenses = float(input("Enter your monthly expenses: "))
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your total monthly expenses:"))
 
-monthly_savings = income - expenses
+monthly_savings = monthly_income - monthly_expenses
 annual_savings = monthly_savings * 12
-interest = annual_savings * 0.05
-projected_savings = annual_savings + interest
-print("Your monthly savings are $", monthly_savings)
-print("Projected savings after one year, with interest, is: $", projected_savings)
+projected_savings = annual_savings + (annual_savings * 0.05)
+
+print("Your monthly savings are", "$" + str(monthly_savings))
+print("projected savings after one year, with interest is:", "$" + str(projected_savings))
